1df46a5d6f13d17a454fe43dc6d42b9f0904db9c
rea <hi@rea.ink>
2024-07-29 21:30:52 +0800

    fix: rm miniconda, add rustup

--- config.fish
+++ config.fish
@@ -6,12 +6,6 @@ source "$HOME/.cargo/env.fish"
 
 fish_vi_key_bindings
 
-# >>> conda initialize >>>
-# !! Contents within this block are managed by 'conda init' !!
-eval /opt/miniconda3/bin/conda "shell.fish" hook $argv | source
-# <<< conda initialize <<<
-
-
 # pnpm
 set -gx PNPM_HOME "/Users/rea-work/Library/pnpm"
 if not string match -q -- $PNPM_HOME $PATH
